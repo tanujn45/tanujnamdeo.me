df450d8c942ec8c93fa109fe248afe996aee386f
tanujn45 <tanujn45@gmail.com>
2024-04-04 10:03:27 -0400

    Generated basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import experienceData from "./json/experience.json";
 
 function App() {
+  const toggleTheme = () => {
+    // Implement your theme toggle logic here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="content">
+      <header>
+        <div className="main">
+          <a href="/">Tanuj Namdeo</a>
+        </div>
+        <nav>
+          <a href="">Home</a>
+          <a href="">All posts</a>
+          <a href="">About</a>
+          <a href="">Tags</a>
+          <span>|</span>
+          <a id="dark-mode-toggle" onClick={toggleTheme} href="/">
+            Toggle Theme
+          </a>
+        </nav>
       </header>
+
+      <main className="list">
+        <section>
+          <h1>Introduction</h1>
+          <ul>
+            <li>
+              Research Associate @ <a>A11y lab</a> | Penn State
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h1>Work Experience</h1>
+          {experienceData.experience.map((job, index) => (
+            <div key={index}>
+              <h2 className="margin-reset">{job.company}</h2>
+              <time>{job.duration}</time>
+              <ul>
+                {job.responsibilities.map((responsibility, i) => (
+                  <li key={i}>{responsibility}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </section>
+      </main>
     </div>
   );
 }
