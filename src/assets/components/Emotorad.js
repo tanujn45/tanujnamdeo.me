f10c8da7926973eb987271416b40326e36b0990b
tanujn45 <tanujn45@gmail.com>
2024-04-10 19:24:29 -0400

    Finished Emotorad Work Experience

--- src/assets/components/Emotorad.js
+++ src/assets/components/Emotorad.js
@@ -9,11 +9,7 @@ const Emotorad = () => {
       <h1>
         <a>Emotorad</a>
       </h1>
-      <time>
-        Software Developer
-        <br />
-      </time>
-      <time>Freelance</time>
+      <time>Software Developer [Freelance]</time>
 
       <section>
         <h2>About Emotorad</h2>
@@ -26,16 +22,28 @@ const Emotorad = () => {
 
       <section>
         <h2>Job Description</h2>
-        <p className="description">
-          Developed a native android application using Java to record Movesense
-          IMU sensor data.
+        <p>
+          Led the development of Emotorad's e-commerce platform, resulting in a
+          37% increase in website traffic within the first three months
+          post-launch.
+        </p>
+        <p>
+          Designed a visually appealing website interface using Figma, which
+          significantly reduced bounce rate and received a notable increase in
+          positive customer feedback on aesthetics and usability.
+        </p>
+        <p>
+          Collaborated cross-functionally to align website features with
+          business goals, contributing to a substantial increase in online sales
+          revenue quarter-over-quarter and expanding market reach through
+          targeted marketing campaigns based on analytics insights.
         </p>
         <div className="row justify-content-center align-items-center mt-4">
           <div className="col-4">
             <figure>
               <img src={homePhone} alt="homePhone" />
               <figcaption>
-                <h4>Bluetooth screen</h4>
+                <h4>Phone screen</h4>
               </figcaption>
             </figure>
           </div>
@@ -43,11 +51,23 @@ const Emotorad = () => {
             <figure>
               <img src={homeDesktop} alt="homeDesktop" />
               <figcaption>
-                <h4>Record data screen</h4>
+                <h4>Desktop screen</h4>
               </figcaption>
             </figure>
           </div>
         </div>
+        <p>
+          Collaborated cross-functionally to align website features with
+          business goals, contributing to a substantial increase in online sales
+          revenue quarter-over-quarter and expanding market reach through
+          targeted marketing campaigns based on analytics insights.
+        </p>
+        <p>
+          Developed Emotorad's e-commerce platform using React, Redux, Django,
+          MongoDB for NoSQL database management, Python for Django backend
+          development, JavaScript for front-end functionality, and Bootstrap CSS
+          for styling, with Figma for design implementation.
+        </p>
       </section>
 
       <ul>
